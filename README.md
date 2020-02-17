57b91dbafdf032ce3e0a94d5c26e195c9c01a80d
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-02-17 17:03:39 +0200

    lab2 commited

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # about-me
+## my name is nermeen nasasra this web page is about me
---
+++ js/app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var userName =prompt('whats your name?');
+alert('welcome '+userName);
+
+document.write("welcome ");
+document.write(userName);
+
+
+var must=confirm("I will ask you som question about me please answer it yes/y or no/n");
+
+ var know=prompt("I am studded a computer enginnering?");
+ console.log(know.toLowerCase);
+
+know.toLowerCase;
+switch(know){
+    case 'yes' :
+    case 'y' :
+        alert('your answer is true');
+        break; 
+    case 'no':
+    case 'n':
+         alert('your answer is false');
+        break;
+    default:
+        alert('wrong answer');
+   
+}
+
+var know=prompt("I am studded at university of jordan?");
+ console.log(know.toLowerCase);
+
+ switch(know){
+    case 'yes' :
+    case 'y' :
+        alert('your answer is true');
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is false');
+        break;
+      
+    default:
+        alert('wrong answer');
+    
+ }
+  
+var know=prompt("I am 23 years old?");
+ console.log(know.toLowerCase);
+
+ switch(know){
+    case 'yes' :
+    case 'y' :
+        alert('your answer is true');
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is false');
+        break;
+    default:
+        alert('wrong answer'); 
+    
+      
+ }
+ var know=prompt("l love sleeping?");
+ console.log(know.toLowerCase);
+
+ switch(know){
+    case 'yes' :
+    case 'y' :
+        alert('your answer is true');
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is false');
+        break;
+    default:
+        alert('wrong answer'); 
+    
+      
+ }
+  
+ var know=prompt("my mum call me narmena?");
+ console.log(know.toLowerCase);
+
+ switch(know){
+    case 'yes' :
+    case 'y' :
+        alert('your answer is true');
+        break;
+    case 'no':
+    case 'n':
+        alert('your answer is false');
+        break;
+      
+    default:
+        alert('wrong answer');
+ }
+  
