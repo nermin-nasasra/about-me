d83d3b06185f08b765734b7db1d7e7dd89eb4c0e
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-02-18 12:06:39 +0200

    lab2 done

--- js/app.js
+++ js/app.js
@@ -1,99 +1,95 @@
 'use strict';
 
-var userName =prompt('whats your name?');
-alert('welcome '+userName);
+var userName = prompt('whats your name?');
+alert('welcome ' + userName);
 
-document.write("welcome ");
+document.write('welcome ');
 document.write(userName);
 
+confirm('I will ask you som question about me please answer it yes/y or no/n');
 
-var must=confirm("I will ask you som question about me please answer it yes/y or no/n");
+var know =prompt('I am studded a computer engineering?');//.toLowerCas();
+console.log(know);
 
- var know=prompt("I am studded a computer enginnering?");
- console.log(know.toLowerCase);
+switch (know.toLowerCase()) {
+case 'yes':
+case 'y':
+  alert('your answer is true');
+  break;
+case 'no':
+case 'n':
+  alert('your answer is false');
+  break;
+default:
+  alert('wrong answer');
 
-know.toLowerCase;
-switch(know){
-    case 'yes' :
-    case 'y' :
-        alert('your answer is true');
-        break; 
-    case 'no':
-    case 'n':
-         alert('your answer is false');
-        break;
-    default:
-        alert('wrong answer');
-   
 }
 
-var know=prompt("I am studded at university of jordan?");
- console.log(know.toLowerCase);
+var study =prompt('I am studded at university of jordan?');//.toLowerCase();
+console.log(study);
 
- switch(know){
-    case 'yes' :
-    case 'y' :
-        alert('your answer is true');
-        break;
-    case 'no':
-    case 'n':
-        alert('your answer is false');
-        break;
-      
-    default:
-        alert('wrong answer');
-    
- }
-  
-var know=prompt("I am 23 years old?");
- console.log(know.toLowerCase);
+switch (study.toLowerCase()) {
+case 'yes':
+case 'y':
+  alert('your answer is true');
+  break;
+case 'no':
+case 'n':
+  alert('your answer is false');
+  break;
 
- switch(know){
-    case 'yes' :
-    case 'y' :
-        alert('your answer is true');
-        break;
-    case 'no':
-    case 'n':
-        alert('your answer is false');
-        break;
-    default:
-        alert('wrong answer'); 
-    
-      
- }
- var know=prompt("l love sleeping?");
- console.log(know.toLowerCase);
+default:
+  alert('wrong answer');
 
- switch(know){
-    case 'yes' :
-    case 'y' :
-        alert('your answer is true');
-        break;
-    case 'no':
-    case 'n':
-        alert('your answer is false');
-        break;
-    default:
-        alert('wrong answer'); 
-    
-      
- }
-  
- var know=prompt("my mum call me narmena?");
- console.log(know.toLowerCase);
+}
 
- switch(know){
-    case 'yes' :
-    case 'y' :
-        alert('your answer is true');
-        break;
-    case 'no':
-    case 'n':
-        alert('your answer is false');
-        break;
-      
-    default:
-        alert('wrong answer');
- }
-  
+var age =prompt('I am 23 years old?');//.toLowerCase();
+console.log(age);
+
+switch (age.toLowerCase()) {
+case 'yes':
+case 'y':
+  alert('your answer is true');
+  break;
+case 'no':
+case 'n':
+  alert('your answer is false');
+  break;
+default:
+  alert('wrong answer');
+
+
+}
+var sleep =prompt('l love sleeping?');//.toLowerCase();
+console.log(sleep);
+
+switch (sleep.toLowerCase()) {
+case 'yes':
+case 'y':
+  alert('your answer is true');
+  break;
+case 'no':
+case 'n':
+  alert('your answer is false');
+  break;
+default:
+  alert('wrong answer');
+
+
+}
+
+var mum =prompt('my mum call me narmena?');//.toLowerCase();
+console.log(mum);
+
+switch (mum.toLowerCase()) {
+case 'yes':
+case 'y':
+  alert('your answer is true');
+  break;
+case 'no':
+case 'n':
+  alert('your answer is false');
+  break;
+default:
+  alert('wrong answer');
+}
