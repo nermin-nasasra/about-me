7257ff497eb38adc1eb158f350ed6d482f73f1be
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-02-18 16:33:40 +0200

    lab3 done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # about-me
 ## my name is nermeen nasasra this web page is about me
+## I am added to this website 2 new question 
--- js/app.js
+++ js/app.js
@@ -1,19 +1,22 @@
 'use strict';
 
+var score = 0;
+
 var userName = prompt('whats your name?');
 alert('welcome ' + userName);
 
-document.write('welcome ');
-document.write(userName);
+//document.write('welcome ');
+//document.write(userName);
 
 confirm('I will ask you som question about me please answer it yes/y or no/n');
 
-var know =prompt('I am studded a computer engineering?');//.toLowerCas();
+var know = prompt('I am studded a computer engineering?');
 console.log(know);
 
 switch (know.toLowerCase()) {
 case 'yes':
 case 'y':
+  score = score + 1;
   alert('your answer is true');
   break;
 case 'no':
@@ -25,12 +28,13 @@ default:
 
 }
 
-var study =prompt('I am studded at university of jordan?');//.toLowerCase();
+var study = prompt('I am studded at university of jordan?');
 console.log(study);
 
 switch (study.toLowerCase()) {
 case 'yes':
 case 'y':
+  score = score + 1;
   alert('your answer is true');
   break;
 case 'no':
@@ -43,12 +47,13 @@ default:
 
 }
 
-var age =prompt('I am 23 years old?');//.toLowerCase();
+var age = prompt('I am 23 years old?');
 console.log(age);
 
 switch (age.toLowerCase()) {
 case 'yes':
 case 'y':
+  score = score + 1;
   alert('your answer is true');
   break;
 case 'no':
@@ -60,12 +65,13 @@ default:
 
 
 }
-var sleep =prompt('l love sleeping?');//.toLowerCase();
+var sleep = prompt('l love sleeping?');
 console.log(sleep);
 
 switch (sleep.toLowerCase()) {
 case 'yes':
 case 'y':
+  score = score + 1;
   alert('your answer is true');
   break;
 case 'no':
@@ -78,12 +84,13 @@ default:
 
 }
 
-var mum =prompt('my mum call me narmena?');//.toLowerCase();
+var mum = prompt('my mum call me narmena?');
 console.log(mum);
 
 switch (mum.toLowerCase()) {
 case 'yes':
 case 'y':
+  score = score + 1;
   alert('your answer is true');
   break;
 case 'no':
@@ -93,3 +100,52 @@ case 'n':
 default:
   alert('wrong answer');
 }
+
+
+
+
+
+
+for (var i = 0; i < 4; i++) {
+  var meet = prompt('How many tiems a week did i meet my friends?');
+  console.log(meet);
+  if (meet == 2) {
+    alert('Yes, i meet them 2 times ber week ');
+    score = score + 1;
+
+    break;
+
+  }
+  else if (meet > 2) {
+    alert('too high ');
+
+  }
+  else if (meet < 2) {
+    alert('too low ');
+
+  }
+}
+
+
+for (var i2 = 0; i2 < 6; i2++) {
+  var arry = ['red', 'black', 'green', 'yallow', 'blue', 'brown'];
+  var color = prompt('choose my favourit color ');
+  console.log(color);
+
+  if (color==arry[0] ||color==arry[1] || color==arry[2] ||color==arry[3] || color==arry[4] ||color==arry[5]) {
+    alert('Yes, this is my favourit color ');
+    score = score + 1;
+    break;
+
+  } else if(color !== arry[0] || color !== arry[2] || color !== arry[3] || color !== arry[4] || color !== arry[5])
+  {
+    alert('Nooo, this is not my favourit color ');
+
+
+  }
+
+}
+alert('my favourit colors :red, black, green, yallow, blue and brown');
+alert('your score is ' + score);
+document.write('welcome ' + name + 'your score ' + score);
+
