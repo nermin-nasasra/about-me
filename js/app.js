b638b4b16acf4f4b8d428b02d3edb90d784f96eb
nermin-nasasra <nermeenmanaseer1496@yahoo.com>
2020-02-27 20:52:56 +0200

    update done

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ favourit();
 
 function me(){
 var know = prompt('I am studded a computer engineering?');
-console.log(know);
+
 
 switch (know.toLowerCase()) {
 case 'yes':
@@ -30,7 +30,7 @@ default:
 }
 
 var study = prompt('I am studded at university of jordan?');
-console.log(study);
+
 
 switch (study.toLowerCase()) {
 case 'yes':
@@ -49,7 +49,7 @@ default:
 }
 
 var age = prompt('I am 23 years old?');
-console.log(age);
+
 
 switch (age.toLowerCase()) {
 case 'yes':
@@ -67,7 +67,7 @@ default:
 
 }
 var sleep = prompt('l love sleeping?');
-console.log(sleep);
+
 
 switch (sleep.toLowerCase()) {
 case 'yes':
@@ -86,7 +86,7 @@ default:
 }
 
 var mum = prompt('my mum call me narmena?');
-console.log(mum);
+
 
 switch (mum.toLowerCase()) {
 case 'yes':
@@ -109,7 +109,6 @@ default:
 function friends (){
 for (var i = 0; i < 4; i++) {
   var meet = prompt('How many tiems a week did i meet my friends?');
-  console.log(meet);
   if (meet == 2) {
     alert('Yes, i meet them 2 times ber week ');
     score = score + 1;
@@ -133,7 +132,7 @@ function favourit(){
 for (var i2 = 0; i2 < 6; i2++) {
   var arry = ['red', 'black', 'green', 'yallow', 'blue', 'brown'];
   var color =prompt('choose my favourit color ').toLowerCase();
-  console.log(color);
+
 
   if (color==arry[0] ||color==arry[1] || color==arry[2] ||color==arry[3] || color==arry[4] ||color==arry[5]) {
     alert('Yes, this is my favourit color ');
